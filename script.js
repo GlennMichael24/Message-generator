62be508f48938e69bfeaf0383e0594f0002557f1
Glenn <glennmichael1992@gmail.com>
2024-04-19 10:26:14 +0200

    added collectiveAdvice to store the strings

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const getRandomNumber = num => {
     return Math.floor(Math.random() * num)
 }
+
+const collectiveAdvice = {
+    colorInfo: ['blue', 'red', 'white', 'black', 'green', 'yellow'],
+    fortuneOutput: ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
+    advice: ['trust no one', 'keep an eye on the sky', 'go out and meet someone', 'today the world is yours']
+}
+
